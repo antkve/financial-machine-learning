ac8976e33dc299698a476369bb0733a5d18a5567
antkve <antkve@github.com>
2018-07-05 23:51:55 +0100

    Finished time bars, tick bars and imbalance bars

--- bars.py
+++ bars.py
@@ -24,25 +24,22 @@ def time_bars(txs, start, sep_secs):
     txs_iter = peekable(iter(txs))
 
     end_date = txs[-1].date
-    print(txs)
     date = txs[0].date
     bars = []
     while date < end_date: 
-        try:
-            date = date + dt.timedelta(seconds=sep_secs)
-            if txs_iter.peek().date < date and date < end_date:
-                bar = Bar(next(txs_iter), start=date)
-                while txs_iter.peek().date < date:
-                    tx = next(txs_iter)
-                    bar.update(tx)
-                bar.close(tx)
-                bars.append(bar)
-        except StopIteration:
-            bar.update(tx)
-            bar.close(tx)
+        date = date + dt.timedelta(seconds=sep_secs)
+        if txs_iter.peek().date < date and date < end_date:
+            bar = Bar(next(txs_iter), start=date)
+            while txs_iter.peek().date < date:
+                tx = next(txs_iter)
+                bar.update(tx)
+            bar.finish(tx)
             bars.append(bar)
-            return bars
 
+    bar.update(tx)
+    bar.finish(tx)
+    bars.append(bar)
+    return bars
 
 def tick_bars(txs, sep):
     txs_iter = iter(txs)
@@ -55,11 +52,11 @@ def tick_bars(txs, sep):
             while bar.ticks < sep:
                 tx = next(txs_iter)
                 bar.update(tx)
-            bar.close(tx)
+            bar.finish(tx)
             bars.append(bar)
         except StopIteration:
             bar.update(tx)
-            bar.close(tx)
+            bar.finish(tx)
             bars.append(bar)
             return bars
 
@@ -75,6 +72,7 @@ def imbalance_bars(txs, counter_threshold,
     E_b_EMA_mult = 2/(b_EMA_span + 1)
     E_b_EMA = 0.5
     T_EMA = 20
+    print(txs)
     prev_rate = txs[0].rate
     diff = 0
     txs_iter = iter(txs)
@@ -87,7 +85,7 @@ def imbalance_bars(txs, counter_threshold,
                 tx = next(txs_iter)
                 bar.update(tx)
                 diff = tx.rate - prev_rate
-                if abs(diff) > counter_threshold * rate:
+                if abs(diff) > counter_threshold * prev_rate:
                     b = sign(diff)
                 else:
                     b = 0
@@ -96,25 +94,25 @@ def imbalance_bars(txs, counter_threshold,
                 E_b_EMA = ((1 + b)/2 - E_b_EMA) * E_b_EMA_mult + E_b_EMA
             
             T_EMA = (bar.ticks - T_EMA) * T_EMA_mult + T_EMA
-            bar.close(tx)
+            bar.finish(tx)
             bars.append(bar)
 
         except StopIteration:
             bar.update(tx)
-            bar.close(tx)
+            bar.finish(tx)
             bars.append(bar)
             return bars
 
         
 def graph(currencypair, start, end):
     polo = Poloniex()
-    txs = get_trade_hist(polo, 'BTC_ETH', start, end),
+    txs = get_trade_hist(polo, 'BTC_ETH', start, end)
     imbalancebars = imbalance_bars(txs, 0.001, 10, 10)
-    timebars = time_bars(txs, start, 10)
-    imbalance_y = [bar.open for bar in bars]
-    imbalance_x = [bar.start for bar in bars]
-    time_y = [bar.open for bar in bars]
-    time_x = [bar.start for bar in bars]
+    timebars = time_bars(txs, start, 200)
+    imbalance_y = [bar.open for bar in imbalancebars]
+    imbalance_x = [bar.start for bar in imbalancebars]
+    time_y = [bar.open for bar in timebars]
+    time_x = [bar.start for bar in timebars]
 
     plt.plot(imbalance_x, imbalance_y, 'b')
     plt.plot(time_x, time_y, 'r')
@@ -122,4 +120,4 @@ def graph(currencypair, start, end):
 
 
 if __name__ == '__main__':
-    graph('BTC_ETH', '2017-05-03T04:00:00', '2017-05-03T04:30:00')
+    graph('BTC_ETH', '2017-05-03T04:00:00', '2017-05-03T16:00:00')
